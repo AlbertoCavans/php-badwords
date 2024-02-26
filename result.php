a26109f2519acfe3fb52061dfb1406d82048b077
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-26 18:13:07 +0100

    Feat result.php - comments and a simple layout

--- result.php
+++ result.php
@@ -1,19 +1,17 @@
 <?php 
+/* var_dump($_POST); */
 
-var_dump($_POST);
+/* RECEIVE PARAGRAPH */
 $sent_paragraph = $_POST['paragraph'];
+/* READ PARAGRAPH LENGTH */
 $length_sent_paragraph = strlen($sent_paragraph);
+/* RECEIVE WORD TO CENSOR */
 $censored_word = $_POST["censored_word"];
+/* CENSOR WORD */
 $censored_text = str_ireplace($censored_word, "*****", $sent_paragraph);
+/* READ CENSORED-PARAGRAPH LENGTH */
 $length_censored_text = strlen($censored_text);
-
-
 ?>
-
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,31 +19,48 @@ $length_censored_text = strlen($censored_text);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Result Bad Words</title>
+
+        <!-- Link Bootstrap CSS -->
+        <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
 </head>
 <body>
+    <h1 class="text-success mt-3 text-center">Print results</h1>
 
-<h5>Paragraph:</h5>
+
+    <div class="row p-5 flex-column">
+        <div class="card col-5 p-4 my-4">
+<h5 class="text-primary">Paragraph:</h5>
+<!-- PRINT PARAGRAPH -->
 <p>
 <?= $sent_paragraph  ?>
 </p>
-
-<h5>Paragraph length:</h5>
+<!-- PRINT PARAGRAPH LENGTH -->
+<h5 class="text-primary">Paragraph length:</h5>
 <p>
 <?= $length_sent_paragraph  ?>
 </p>
+</div>
 
 
-<hr>
-
-<h5>Censored text:</h5>
+<div class="card col-5 p-4">
+<!-- PRINT CENSORED-PARAGRAPH -->
+<h5 class="text-primary">Censored text:</h5>
 <p>
 <?= $censored_text  ?>
 </p>
-
-<h5>Paragraph length:</h5>
+<!-- PRINT CENSORED-PARAGRAPH-LENGTH -->
+<h5 class="text-primary">Paragraph length:</h5>
 <p>
 <?= $length_censored_text  ?>
 </p>
+</div>
+
+</div>
 
 
 </body>
