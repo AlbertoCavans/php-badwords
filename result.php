7d8ef6ab7cf77a33794765ae0cff23e9812794d1
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-26 17:15:12 +0100

    Feat result.php - censor for dolor word & words count

--- result.php
+++ result.php
@@ -2,7 +2,11 @@
 
 var_dump($_POST);
 $sent_paragraph = $_POST['paragraph'];
-$length_sent_paragraph = strlen($sent_paragraph)
+$length_sent_paragraph = strlen($sent_paragraph);
+$censored_word = $_POST["censored_word"];
+$censored_text = str_ireplace($censored_word, "*****", $sent_paragraph);
+$length_censored_text = strlen($censored_text);
+
 
 ?>
 
@@ -29,6 +33,20 @@ $length_sent_paragraph = strlen($sent_paragraph)
 <p>
 <?= $length_sent_paragraph  ?>
 </p>
+
+
+<hr>
+
+<h5>Censored text:</h5>
+<p>
+<?= $censored_text  ?>
+</p>
+
+<h5>Paragraph length:</h5>
+<p>
+<?= $length_censored_text  ?>
+</p>
+
 
 </body>
 </html>
